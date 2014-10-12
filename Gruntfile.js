81f60c42de903ab12eb25921e127113a51f7a218
Luis Fortes <fortesl@yahoo.com>
2014-10-11 20:52:51 -0500

    Added grunt dist task, which serves optimized resources

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,21 @@
 (function () {
     'use strict';
 
+    var fs = require('fs');
+
     module.exports = function(grunt) {
 
         grunt.loadNpmTasks('grunt-contrib-jshint');
         grunt.loadNpmTasks('grunt-contrib-watch');
         grunt.loadNpmTasks('grunt-contrib-connect');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-contrib-cssmin');
+        grunt.loadNpmTasks('grunt-contrib-htmlmin');
+        grunt.loadNpmTasks('grunt-jsonmin');
+        grunt.loadNpmTasks('grunt-contrib-concat-sourcemaps');
+        grunt.loadNpmTasks('grunt-processhtml');
+        grunt.loadNpmTasks('grunt-angular-templates');
 
         grunt.initConfig({
 
@@ -20,8 +30,94 @@
             srchtmlFiles: ['app/**/*.html'],
             srccssFiles: ['app/css/**/*.css'],
 
+            // For dist, concat to 1 js resource to reduce the number of requests.
+            concat: {
+                people: {
+                    dest: 'dist/js/people.min.js',
+                    src: '<%= srcjsFiles %>'
+                }
+            },
 
-            // The actual grunt server settings
+            // For dist, prepare dist index.html
+            processhtml: {
+                people: {
+                    files: {'dist/index.html': ['app/index.html']}
+                }
+            },
+
+            // For dist, minimize json, css, html, and js resources, to reduce number of bytes loaded
+            jsonmin: {
+                people: {
+                    files: [ {expand: true, cwd: 'app/js/server/dataStore', src: ['**/*.json'], dest: 'dist/js/server/dataStore/'} ]
+                }
+            },
+
+            cssmin: {
+                people: {
+                    files: {
+                        'dist/css/people.min.css': ['<%= srccssFiles %>']
+                    }
+                }
+            },
+
+            htmlmin: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    removeAttributeQuotes: true,
+                    removeRedundantAttributes: true,
+                    removeOptionalTags: true,
+                    removeScriptTypeAttributes:     true,
+                    removeStyleLinkTypeAttributes:  true,
+                    removeEmptyAttributes:          true,
+                    minifyJS: true,
+                    minifyCSS: true,
+                    minifyURLs: true
+                },
+                index: {
+                    files: {'dist/index.html': 'dist/index.html'}
+                }
+            },
+
+            uglify: {
+                options: {
+                    mangle: true,
+                    preserveComments: false,
+                    compress: true
+                },
+                js: {
+                    files: {'<%= concat.people.dest %>': ['<%= concat.people.dest %>']}
+                }
+            },
+
+            ngtemplates:  {
+                peopleApp:        {
+                    cwd: 'app',
+                    src:      'views/**/*.html',
+                    dest:     'app/js/templates.js',
+                    options: {
+                        htmlmin: {
+                            removeComments: true,
+                            collapseWhitespace: true,
+                            collapseBooleanAttributes: true,
+                            removeAttributeQuotes: true,
+                            removeRedundantAttributes: true,
+                            removeOptionalTags: true,
+                            removeScriptTypeAttributes: true,
+                            removeStyleLinkTypeAttributes: true,
+                            removeEmptyAttributes: true
+                        }
+                    }
+                }
+            },
+
+            clean: {
+                build: ['dist'],
+                postbuild: ['<%= ngtemplates.peopleApp.dest %>']
+            },
+
+            // The grunt server settings for running the app
             connect: {
                 options: {
                     // Change this to '0.0.0.0' to access the server from outside.
@@ -33,6 +129,15 @@
                         port: 9002,
                         open: true,
                         base: 'app'
+                    }
+                },
+                dist: {
+                    options: {
+                        livereload: true,
+                        keepalive: true,
+                        port: 9004,
+                        open: true,
+                        base: 'dist'
                     }
                 }
             },
@@ -66,6 +171,21 @@
 
         }); //initConfig
 
+        grunt.registerTask('log-build', function() {
+            this.requires('ngtemplates');
+            this.requires('clean:build');
+            this.requires('concat');
+            this.requires('processhtml');
+            this.requires('jsonmin');
+            this.requires('cssmin');
+            this.requires('htmlmin');
+            this.requires('lintjs');
+            this.requires('uglify');
+            grunt.task.run('clean:postbuild');
+            var message = 'Built on ' + new Date();
+            fs.appendFileSync('build.log', message + '\n');
+            grunt.log.writeln(message);
+        });
 
         // makes jshint optional
         grunt.registerTask('lintjs', function() {
@@ -77,13 +197,16 @@
             }
         });
 
-        //run webapp
+        //run source app
         grunt.registerTask('serve', 'start a connect web server', function () {
             grunt.task.run([
                 'connect:livereload',
                 'watch'
             ]);
         });
+
+        grunt.registerTask('dist', ['ngtemplates', 'clean:build', 'concat', 'processhtml', 'jsonmin', 'cssmin', 'htmlmin', 'lintjs', 'uglify', 'log-build', 'connect:dist']);
+
 
     }; //module.export
 
